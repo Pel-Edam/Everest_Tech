7a477c2ff2820ddbe749ad067e366a18c4efe3f7
Pel_3dam <pelumiedamisan@gmail.com>
2026-06-01 10:43:24 +0100

    Added admin page for viewing current and completed tasks

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
-const hide= document.querySelector('#hide')
+const hide1= document.querySelector('#hide1')
+const hide2= document.querySelector('#hide2')
 const loginBtn = document.querySelector('#logIn')
 loginBtn.addEventListener('click',login)
 function login(){
    const input1 = document.querySelector('#check1').value.trim().toLowerCase()
    const input2 = document.querySelector('#check2').value.trim().toLowerCase()
-    if (input1 === "aura group"  && input2 === "typeshii" || input1 === "everest_admin" && input2 === "aura") {
-        alert('Correct!')
-        hide.classList.remove('hidden')
+    if (input1 === "aura group"  && input2 === "typeshii") {
+        alert('Welcome, US3R!')
+        hide1.classList.remove('hidden')
 
-    }else{
-        hide.classList.add('hidden')
-        alert('Access Denied!!')
+    }else if(input1 === "everest_admin" && input2 === "aura"){
+        alert('Welcome ADM1N!!')
+        hide2.classList.remove('hidden')
+    }
+    else{
+        alert('Incorrect!!')
+        hide1.classList.add('hidden')
     }
 }
 function toggleDarkMode() {
