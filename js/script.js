06ceed409bd2b5bee8eef2c9f141a434b6f713b3
Pel_3dam <pelumiedamisan@gmail.com>
2026-06-02 07:16:47 +0100

    Admin Index and script.js update

--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@ const hide2= document.querySelector('#hide2')
 const loginBtn = document.querySelector('#logIn')
 loginBtn.addEventListener('click',login)
 function login(){
-   const input1 = document.querySelector('#check1').value.trim().toLowerCase()
-   const input2 = document.querySelector('#check2').value.trim().toLowerCase()
-    if (input1 === "aura group"  && input2 === "typeshii") {
+   const user_name = document.querySelector('#check1').value.trim().toLowerCase()
+   const passkey = document.querySelector('#check2').value.trim().toLowerCase()
+    if (user_name === "everest_user"  && passkey === "1234") {
         alert('Welcome, US3R!')
         hide1.classList.remove('hidden')
 
-    }else if(input1 === "everest_admin" && input2 === "aura"){
+    }else if(user_name === "everest_admin" && passkey === "aura"){
         alert('Welcome ADM1N!!')
         hide2.classList.remove('hidden')
     }
