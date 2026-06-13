13e17258222cebe4a45e53537dac7aee8af483f1
Pel_3dam <pelumiedamisan@gmail.com>
2026-06-13 07:26:51 +0100

    Modifications and Simplifications

--- js/script.js
+++ js/script.js
@@ -5,16 +5,11 @@ loginBtn.addEventListener('click',login)
 function login(){
    const user_name = document.querySelector('#check1').value.trim().toLowerCase()
    const passkey = document.querySelector('#check2').value.trim().toLowerCase()
-    if (user_name === "everest_user"  && passkey === "1234") {
-        alert('Welcome, US3R!')
-        hide1.classList.remove('hidden')
-
-    }else if(user_name === "everest_admin" && passkey === "aura"){
+    if(user_name === "everest_admin" && passkey === "aura"){
         alert('Welcome ADM1N!!')
         hide2.classList.remove('hidden')
     }
     else{
-        alert('Incorrect!!')
         hide1.classList.add('hidden')
     }
 }
